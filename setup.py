a14722212d38de623557305d7c05a468b8bc8c38
Tres Bailey <tres.bailey@gmail.com>
2012-05-17 01:38:55 -0400

    Using list from reqs.txt

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 
+reqs_list = list()
+with open('./requirements.txt', 'r') as reqs:
+    for line in reqs.readlines():
+        reqs_list.append(line)
+
+
 setup(name='YourAppName',
       version='1.0',
       description='OpenShift App',
       author='Your Name',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['flask', 'beautifulsoup4==4.0.5'],
+      install_requires=reqs_list,
      )
