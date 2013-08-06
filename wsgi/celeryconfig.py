d192dd0504521d7df6e98982fdd1cceb656ce50e
tresbailey <tres.bailey@gmail.com>
2013-08-06 13:24:34 -0400

    celeryconfig for cartridge startup

--- wsgi/celeryconfig.py
+++ wsgi/celeryconfig.py
@@ -1,11 +1,13 @@
+from scribble import app
+
 ## Broker settings.
-BROKER_URL = 'mongodb://localhost:27017/celery_tasks'
+BROKER_URL = 'mongodb://%s:%d/celery_tasks' % (app.config['MONGOALCHEMY_SERVER'], app.config['MONGOALCHEMY_PORT'])
 
 # List of modules to import when celery starts.
 CELERY_IMPORTS = ("scribble.views.scribbles", )
 
 ## Using the database to store task state and results.
 CELERY_RESULT_BACKEND = "mongodb"
-CELERY_RESULT_DBURI = 'mongodb://localhost:27017/celery_tasks'
+CELERY_RESULT_DBURI = 'mongodb://%s:%d/celery_tasks' % (app.config['MONGOALCHEMY_SERVER'], app.config['MONGOALCHEMY_PORT'])
 
 CELERY_ANNOTATIONS = {"tasks.add": {"rate_limit": "10/s"}}
