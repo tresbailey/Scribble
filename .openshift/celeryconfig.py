c7b8ea5439149a5605e09cc3dfd5688091687fe5
tresbailey <tres.bailey@gmail.com>
2013-08-10 22:45:19 -0400

    Removing app references in celery config

--- .openshift/celeryconfig.py
+++ .openshift/celeryconfig.py
@@ -1,13 +1,17 @@
-from scribble import app
+import os
 
+print 'Configuring celery cartridge with client override'
+
+app.config['MONGOALCHEMY_SERVER'] = os.environ.get('', 'localhost')
+app.config['MONGOALCHEMY_PORT'] = int(os.environ.get('', 27017))
 ## Broker settings.
-BROKER_URL = 'mongodb://%s:%d/celery_tasks' % (app.config['MONGOALCHEMY_SERVER'], app.config['MONGOALCHEMY_PORT'])
+BROKER_URL = 'mongodb://%s:%d/celery_tasks' % (os.environ.get('OPENSHIFT_MONGODB_DB_HOST'), os.environ.get('OPENSHIFT_MONGODB_DB_PORT'))
 
 # List of modules to import when celery starts.
 CELERY_IMPORTS = ("scribble.views.scribbles", )
 
 ## Using the database to store task state and results.
 CELERY_RESULT_BACKEND = "mongodb"
-CELERY_RESULT_DBURI = 'mongodb://%s:%d/celery_tasks' % (app.config['MONGOALCHEMY_SERVER'], app.config['MONGOALCHEMY_PORT'])
+CELERY_RESULT_DBURI = 'mongodb://%s:%d/celery_tasks' % (os.environ.get('OPENSHIFT_MONGODB_DB_HOST'), os.environ.get('OPENSHIFT_MONGODB_DB_PORT'))
 
 CELERY_ANNOTATIONS = {"tasks.add": {"rate_limit": "10/s"}}
