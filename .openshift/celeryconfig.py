6cca1ab5d921b363f04f5c768481421e67e1f4b7
tresbailey <tres.bailey@gmail.com>
2013-08-10 23:18:19 -0400

    Trying to remove ascii encodes of the broker urls

--- .openshift/celeryconfig.py
+++ .openshift/celeryconfig.py
@@ -3,13 +3,13 @@ import os
 print 'Configuring celery cartridge with client override'
 
 ## Broker settings.
-BROKER_URL = 'mongodb://%s:%d/celery_tasks' % (os.environ.get('OPENSHIFT_MONGODB_DB_HOST'), int(os.environ.get('OPENSHIFT_MONGODB_DB_PORT')))
+BROKER_URL = "mongodb://%s:%s/celery_tasks" % (os.environ.get('OPENSHIFT_MONGODB_DB_HOST'), os.environ.get('OPENSHIFT_MONGODB_DB_PORT'))
 
 # List of modules to import when celery starts.
 CELERY_IMPORTS = ("scribble.views.scribbles", )
 
 ## Using the database to store task state and results.
 CELERY_RESULT_BACKEND = "mongodb"
-CELERY_RESULT_DBURI = 'mongodb://%s:%d/celery_tasks' % (os.environ.get('OPENSHIFT_MONGODB_DB_HOST'), int(os.environ.get('OPENSHIFT_MONGODB_DB_PORT')))
+CELERY_RESULT_DBURI = "mongodb://%s:%s/celery_tasks" % (os.environ.get('OPENSHIFT_MONGODB_DB_HOST'), os.environ.get('OPENSHIFT_MONGODB_DB_PORT'))
 
 CELERY_ANNOTATIONS = {"tasks.add": {"rate_limit": "10/s"}}
