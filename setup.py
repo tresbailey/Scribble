43a0e6c1c4f610d4e5cf4fb89bb23b7265b2e176
tresbailey <tres.bailey@gmail.com>
2013-10-22 22:39:47 -0400

    Updating requirements with good syntax

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(name='WriteOwn', version='1.0',
       #  Uncomment one or more lines below in the install_requires section
       #  for the specific client drivers/modules your application needs.
       install_requires=[ 'Flask==0.8', 'MongoAlchemy==0.11',
-        'Werkzeug==0.8.3', 'Flask-Login==0.2.7', 'Flask-OpenId=1.1.1',
+        'Werkzeug==0.8.3', 'Flask-Login==0.2.7', 'Flask-OpenId==1.1.1',
         'pymongo==2.1.1', 'python-dateutil==1.5', 
         'redis==2.4.11', 'simplejson==2.1.6', 
         'wsgiref==0.1.2',
