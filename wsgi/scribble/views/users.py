7cb5f47d02e57c0226862e09a64af790822b3f68
tresbailey <tres.bailey@gmail.com>
2013-11-04 22:49:30 -0500

    Flask principal setup

--- wsgi/scribble/views/users.py
+++ wsgi/scribble/views/users.py
@@ -2,6 +2,7 @@ import json
 import requests
 from flask import Blueprint, render_template, send_from_directory, request, jsonify, url_for, g, redirect, make_response
 from flask.ext.login import login_user, logout_user, current_user, login_required
+from flask.ext.principal import identity_loaded, RoleNeed, UserNeed, Identity, identity_changed
 from scribble import app, lm, oid, HOME_URL
 from pymongo.objectid import ObjectId
 from scribble.storage.models import Scruser
@@ -9,21 +10,26 @@ from scribble.storage.models import Scruser
 users = Blueprint('login_pages', __name__,
         template_folder='scribble/templates', static_folder='static')
 
-@users.route('/app_callback', methods=['POST'])
-def app_callback():
+def janrain_data():
     token = request.form['token']
     engage_api_params = dict(
-        apiKey='f2e3e9fe58895c89a1bec0bd0d2326cf30ae4d5d',
+        apiKey='f2e3e9fe58895c89a1bec0bd0d2326cf30ae4d5d', 
         token=token
     )
     user_data = requests.get("https://writeown.rpxnow.com/api/v2/auth_info", params=engage_api_params)
-    auth_info = json.loads(user_data.text)
+    return json.loads(user_data.text)
+
+
+@users.route('/app_callback', methods=['POST'])
+def app_callback():
     name = auth_info['profile']['name']['formatted']
     user = Scruser.query.filter(
-            Scruser.open_id == auth_info['profile']['identifier']
+            Scruser.open_id == {auth_info['profile']['providerSpecifier']: auth_info['profile']['identifier']}
         ).one()
     login_user(user, remember=True)
     g.user = user
+    identity = Identity(user.mongo_id)
+    identity_changed.send(app, identity=identity)
     return redirect(HOME_URL +'/'+ user.get_id())
 
 @users.route('/create_user', methods=['POST'])
